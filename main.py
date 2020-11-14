e8dce986bfe75d4e4fbff8dec0eca95dc9affe32
Alif <mail@ahmedalif.com>
2020-11-14 06:23:48 +0600

    fixes

--- main.py
+++ main.py
@@ -29,46 +29,29 @@ scissors = '''
 
 option = [rock, paper, scissors]
 
-yourChoice = int(input("What do you choose? Type 0 for rock, 1 for Paper or 2 for Scissors. "))
+yourChoice = int(input("What do you choose? Type 0 for rock, 1 for Paper or 2 for Scissors.\n"))
+if yourChoice >=3 or yourChoice <0:
+  print("You typed an invalid input, You lose!")
+  exit()
+print(option[yourChoice])
 
 computerChoice = random.randint(0,2)
-
-
+print("Computer chose:")
+print(option[computerChoice])
 
 if yourChoice == computerChoice:
-  print(option[yourChoice])
-  print("Computer chose:")
-  print(option[computerChoice])
   print("Draw")
 elif yourChoice == 0 and computerChoice == 2:
-  print(option[yourChoice])
-  print("Computer chose:")
-  print(option[computerChoice])
   print("You win!")
 elif yourChoice == 0 and computerChoice == 1:
-  print(option[yourChoice])
-  print("Computer chose:")
-  print(option[computerChoice])
   print("You lose!")
 elif yourChoice == 1 and computerChoice == 0:
-  print(option[yourChoice])
-  print("Computer chose:")
-  print(option[computerChoice])
   print("You win!")
 elif yourChoice == 1 and computerChoice == 2:
-  print(option[yourChoice])
-  print("Computer chose:")
-  print(option[computerChoice])
   print("You lose!")
 elif yourChoice == 2 and computerChoice == 0:
-  print(option[yourChoice])
-  print("Computer chose:")
-  print(option[computerChoice])
   print("You lose!")
 elif yourChoice == 2 and computerChoice == 1:
-  print(option[yourChoice])
-  print("Computer chose:")
-  print(option[computerChoice])
   print("You win!")
 else:
   print("Error")
